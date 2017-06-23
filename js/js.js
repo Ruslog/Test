88d0c98e5d1700aaf5228ca67dc83b2e46fed42f
Ruslog <ruslan.logvin1988@gmail.com>
2017-06-23 14:58:03 +0300

    1

---
+++ js/js.js
@@ -0,0 +1,170 @@
+document.onwheel = function (event) {
+	var z = document.getElementById('z');
+	var y = document.getElementById('y');
+	var x = document.getElementById('x');
+	var w = document.getElementById('w');
+	var v = document.getElementById('v');
+	var u = document.getElementById('u');
+	var t = document.getElementById('t');
+	var s = document.getElementById('s');
+	var r = document.getElementById('r');
+	var q = document.getElementById('q');
+	var p = document.getElementById('p');
+	var o = document.getElementById('o');
+	var n = document.getElementById('n');
+	var m = document.getElementById('m');
+	var l = document.getElementById('l');
+	var k = document.getElementById('k');
+	var j = document.getElementById('j');
+	var i = document.getElementById('i');
+	var h = document.getElementById('h');
+	var g = document.getElementById('g');
+	var f = document.getElementById('f');
+	var e = document.getElementById('e');
+	var d = document.getElementById('d');
+	var c = document.getElementById('c');
+	var b = document.getElementById('b');
+	var a = document.getElementById('a');
+	if(event.deltaY>0){
+		z.style.background="#F47180";
+		z.style.transition="ease 2s all";
+		y.style.background="#F47180";
+		y.style.transition="ease 2s all";
+		x.style.background="#F47180";
+		x.style.transition="ease 2s all";
+		w.style.background="#F47180";
+		w.style.transition="ease 2s all";
+		v.style.background="#F47180";
+		v.style.transition="ease 2s all";
+		u.style.background="#F47180";
+		u.style.transition="ease 2s all";
+		t.style.background="#F47180";
+		t.style.transition="ease 2s all";
+		s.style.background="#F47180";
+		s.style.transition="ease 2s all";
+		r.style.background="#F47180";
+		r.style.transition="ease 2s all";
+		q.style.background="#F47180";
+		q.style.transition="ease 2s all";
+		p.style.background="#F47180";
+		p.style.transition="ease 2s all";
+		o.style.background="#F47180";
+		o.style.transition="ease 2s all";
+		n.style.background="#F47180";
+		n.style.transition="ease 2s all";
+		m.style.background="#F47180";
+		m.style.transition="ease 2s all";
+		l.style.background="#F47180";
+		l.style.transition="ease 2s all";
+		k.style.background="#F47180";
+		k.style.transition="ease 2s all";
+		j.style.background="#F47180";
+		j.style.transition="ease 2s all";
+		i.style.background="#F47180";
+		i.style.transition="ease 2s all";
+		h.style.background="#F47180";
+		h.style.transition="ease 2s all";
+		g.style.background="#F47180";
+		g.style.transition="ease 2s all";
+		f.style.background="#F47180";
+		f.style.transition="ease 2s all";
+		e.style.background="#F47180";
+		e.style.transition="ease 2s all";
+		d.style.background="#F47180";
+		d.style.transition="ease 2s all";
+		c.style.background="#F47180";
+		c.style.transition="ease 2s all";
+		b.style.background="#F47180";
+		b.style.transition="ease 2s all";
+		a.style.background="#F47180";
+		a.style.transition="ease 2s all";
+	} else {
+		z.style.background="#92FF85";
+		z.style.transition="ease 2s all";
+		y.style.background="#92FF85";
+		y.style.transition="ease 2s all";
+		x.style.background="#92FF85";
+		x.style.transition="ease 2s all";
+		w.style.background="#92FF85";
+		w.style.transition="ease 2s all";
+		v.style.background="#92FF85";
+		v.style.transition="ease 2s all";
+		u.style.background="#92FF85";
+		u.style.transition="ease 2s all";
+		t.style.background="#92FF85";
+		t.style.transition="ease 2s all";
+		s.style.background="#92FF85";
+		s.style.transition="ease 2s all";
+		r.style.background="#92FF85";
+		r.style.transition="ease 2s all";
+		q.style.background="#92FF85";
+		q.style.transition="ease 2s all";
+		p.style.background="#92FF85";
+		p.style.transition="ease 2s all";
+		o.style.background="#92FF85";
+		o.style.transition="ease 2s all";
+		n.style.background="#92FF85";
+		n.style.transition="ease 2s all";
+		m.style.background="#92FF85";
+		m.style.transition="ease 2s all";
+		l.style.background="#92FF85";
+		l.style.transition="ease 2s all";
+		k.style.background="#92FF85";
+		k.style.transition="ease 2s all";
+		j.style.background="#92FF85";
+		j.style.transition="ease 2s all";
+		i.style.background="#92FF85";
+		i.style.transition="ease 2s all";
+		h.style.background="#92FF85";
+		h.style.transition="ease 2s all";
+		g.style.background="#92FF85";
+		g.style.transition="ease 2s all";
+		f.style.background="#92FF85";
+		f.style.transition="ease 2s all";
+		e.style.background="#92FF85";
+		e.style.transition="ease 2s all";
+		d.style.background="#92FF85";
+		d.style.transition="ease 2s all";
+		c.style.background="#92FF85";
+		c.style.transition="ease 2s all";
+		b.style.background="#92FF85";
+		b.style.transition="ease 2s all";
+		a.style.background="#92FF85";
+		a.style.transition="ease 2s all";
+	}
+}
+
+
+var data = [5, 100, 10, 120, 50, 250, 750, 70, 30, 330, 220, 255, 77, 666, 333, 133, 111, 166, 203, 278, 14, 707, 22, 567, 747, 679];
+function sort(button){	
+	 console.dir(data.sort());
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
